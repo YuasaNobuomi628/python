7a35d6045416b4133e8825dfd4629fbebb37b809
YuasaNobuomi628 <nobuomi628@gmail.com>
2024-05-25 14:31:40 -0400

    Update main.py

--- homework/homework1/main.py
+++ homework/homework1/main.py
@@ -3,16 +3,24 @@
 # Numbers, Variables, Data Types, Input
 
 # Task 1: Print Sum
-print()
+print(11+14+17)
+
 
 # Task 2: Print Hours
-print()
+print(24 * 365)
+
 
 # Task 3: Repeating Greeting
-print()
+print("hello" * 11)
+
 
 # Task 4: Convert string to float
-print(type("25.7"))
+print(type("25.7")
+    a = "25.7"
+b = float(a)
+print(type(a), ":", b)
+
 
 # Task 5: Un-stringify
 print("12" + "4")
+12 + 4
